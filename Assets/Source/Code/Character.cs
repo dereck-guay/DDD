be15f50c83f52268c1d72b6286a1913b9997fb56
unknown <dedeguay@gmail.com>
2020-02-01 09:41:23 -0500

    trying to fix merge conflict

--- Assets/Source/Code/Character.cs
+++ Assets/Source/Code/Character.cs
@@ -4,21 +4,6 @@ using System.Text;
 
 public class Character
 {
-<<<<<<< HEAD
-    public class Character
-    {
-        #region Props
-        public Mana Mana { get; private set; }
-        public HP HP { get; private set; }
-        public AtkSpeed AtkSpeed { get; private set; }
-        public Speed Speed { get; private set; }
-        public Classes Class { get; private set; }
-        #endregion
-        
-        #region Methods
-        #endregion
-    }
-=======
     #region Props
     public Mana Mana { get; private set; }
     public HP HP { get; private set; }
@@ -30,5 +15,4 @@ public class Character
 
     #region Methods
     #endregion
->>>>>>> 02b2f579142e807b9aaecb39187c0e37b8ce0c73
 }
--- Assets/Source/Code/Classes.cs
+++ Assets/Source/Code/Classes.cs
@@ -2,23 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
-<<<<<<< HEAD
-
-public class Classes
-{
-
-}
-public class Fighter
-=======
-public class Classes
-{
-}
 public class Fighter
 {
 
 }
-public class Wizard
->>>>>>> 02b2f579142e807b9aaecb39187c0e37b8ce0c73
+public class Classes
 {
 
 }
