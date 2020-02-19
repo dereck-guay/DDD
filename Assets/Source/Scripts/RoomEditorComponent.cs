8264ea0af28b93b516dc2ef01e6dfb8c9b1e1255
Arasmith <yanericjerome@gmail.com>
2020-02-19 12:52:49 -0500

    Plus de travail sur RoomGenerator

---
+++ Assets/Source/Scripts/RoomEditorComponent.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public struct Tile
+{
+   public int Value { get; }
+   public string Name { get => texture.name; }
+   Texture texture;
+
+   public Tile(int valueI, Texture textureI)
+   {
+      Value = valueI;
+      texture = textureI;
+   }
+}
+
+public class RoomEditorComponent : MonoBehaviour
+{
+   int[,] roomLayout;
+   Tile[] tiles;
+   Tile currentTile;
+   public string patternsPath, tilesPath;
+
+   public Button saveButton;
+   public Toggle bossRoomToggle,
+                 northToggle,
+                 westToggle,
+                 eastToggle,
+                 southToggle;
+   public Dropdown tileSelector;
+
+   void Awake()
+   {
+      GenerateTiles(Resources.LoadAll<Texture>(tilesPath));
+   }
+
+   void Start()
+   {
+      tileSelector.onValueChanged.AddListener(delegate { currentTile = tiles[tileSelector.value]; });
+      tileSelector.onValueChanged.AddListener(delegate { Debug.Log(currentTile.Value); });
+
+      tileSelector.AddOptions(tiles.Select(tile => tile.Name).ToList());
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      
+   }
+
+   void GenerateTiles(Texture[] sprites)
+   {
+      tiles = new Tile[sprites.Length];
+
+      for (int i = 0; i < tiles.Length; ++i)
+         tiles[i] = new Tile(i, sprites[i]);
+   }
+}
