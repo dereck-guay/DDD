82d1058220a53b118e97b30524c9029a92d33d94
Yan-Éric Jérôme <201846087@edu.clg.qc.ca>
2020-03-09 10:40:41 -0400

    Map avec toutes les fonctionnalités de base!
    Turret et Hazard sont 100% fonctionnels.

--- Assets/Source/Scripts/Collisions/TurretCollisionComponent.cs
+++ Assets/Source/Scripts/Collisions/TurretCollisionComponent.cs
@@ -6,6 +6,12 @@ public class TurretCollisionComponent : CollisionMonoBehaviour
 {
    public int[] damageLayers;
    public float damage;
+   public float maxDuration;
+
+   float currentTime;
+   Vector3 baseScale;
+
+   private void Start() => baseScale = gameObject.transform.localScale;
 
    private void OnCollisionEnter(Collision collision)
    {
@@ -17,5 +23,15 @@ public class TurretCollisionComponent : CollisionMonoBehaviour
          if (CollidesWithAppropriateLayer(target.layer, damageLayers))
             target.GetComponent<Stats>().HP.TakeDamage(damage);
       }
+   }
+
+   private void Update()
+   {
+      currentTime += Time.deltaTime;
+
+      gameObject.transform.localScale = baseScale * Mathf.Sqrt(Mathf.Abs(maxDuration - currentTime) / maxDuration);
+
+      if (currentTime >= maxDuration)
+         Destroy(gameObject);
    }
 }
