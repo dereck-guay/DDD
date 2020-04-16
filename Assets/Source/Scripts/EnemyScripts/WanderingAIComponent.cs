6f83560fa9217b22e00821d2078a3351cfd4522e
Arasmith <yanericjerome@gmail.com>
2020-04-15 21:38:48 -0400

    WanderingAI !
    Il y a encore quelques problèmes (la console est spammée par des messages d'erreur...), mais tout fonctionne! Il y a beaucoup plus de vie dans le donjon!

--- Assets/Source/Scripts/EnemyScripts/WanderingAIComponent.cs
+++ Assets/Source/Scripts/EnemyScripts/WanderingAIComponent.cs
@@ -7,6 +7,11 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class WanderingAIComponent : MonoBehaviour
 {
+    const float DistanceBufferMultiplier = 3;
+
+    public bool staysNearSpawn;
+    public float radius;
+
     public FloatInterval delay;
     [HideInInspector]
     public NavMeshAgent agent;
@@ -14,34 +19,55 @@ public class WanderingAIComponent : MonoBehaviour
     public bool isStunned;
 
     float cooldown = 0;
+    Vector3 spawnPosition;
+    WalkingAnimationComponent walkingAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        TryGetComponent(out walkingAnimation);
+        spawnPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (agent.enabled && agent.remainingDistance == 0)
+        {
+            agent.enabled = false;
+            walkingAnimation?.Stop();
+
+        }
+
         if (!isStunned)
         {
             cooldown += Time.deltaTime;
-
             if (cooldown >= delay.Current)
             {
                 cooldown -= delay.Current;
                 delay.NewRandomValue();
 
-                SetDestination();
+                agent.enabled = true;
+                StartCoroutine("SetDestination");
+                walkingAnimation?.Walk();
             }
-        }        
+        }         
     }
 
-    public Vector3 SetDestination()
+    IEnumerator SetDestination()
     {
-        //agent.SetDestination(Vector3.zero);
-        return Vector3.zero;
+        Vector3 destination;
+        float angle, distance;
+        do
+        {
+            angle = Random.Range(0, 2 * Mathf.PI);
+            distance = Random.Range(staysNearSpawn ? 0 : radius / 2, radius);
+            destination = (staysNearSpawn ? spawnPosition : transform.position) + new Vector3(Mathf.Cos(angle), transform.position.y, Mathf.Sin(angle)) * distance;
+            agent.SetDestination(destination);
+            yield return null;
+        }
+        while (agent.remainingDistance > DistanceBufferMultiplier * distance);
     }
 
     public void Go()
