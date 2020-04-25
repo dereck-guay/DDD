a41337e6510bbdb4604316318994d41d08fb329f
Dereck Guay <dedeguay@gmail.com>
2020-04-25 12:45:44 -0400

    Connect to cae Photon server

---
+++ Assets/Source/Scripts/Managers/NetworkManagerComponent.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Concepts tiré de la série Youtube : https://www.youtube.com/watch?v=02P_mrszvzY
+public class NetworkManagerComponent : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to: " + PhotonNetwork.CloudRegion);
+    }
+}
