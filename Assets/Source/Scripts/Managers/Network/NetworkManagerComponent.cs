715c7aef76cf8f7cc2cd52149cf5d7370f2bbc8e
Dereck Guay <dedeguay@gmail.com>
2020-04-29 17:54:52 -0400

    Func. des différents scripts de network fusionner dans NetworkManager

--- Assets/Source/Scripts/Managers/Network/NetworkManagerComponent.cs
+++ Assets/Source/Scripts/Managers/Network/NetworkManagerComponent.cs
@@ -3,16 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Concepts tiré de la série Youtube : https://www.youtube.com/watch?v=02P_mrszvzY
 public class NetworkManagerComponent : MonoBehaviourPunCallbacks
 {
-    private void Start()
+    // Instance accessible partout dans le code NetworkManagerComponent.instance;
+    public static NetworkManagerComponent instance;
+
+    private void Awake()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Garde le gameObject quand on change de scene.
+        }
     }
 
-    public override void OnConnectedToMaster()
-    {
+    private void Start() =>
+        PhotonNetwork.ConnectUsingSettings();
+    public override void OnConnectedToMaster() =>
         Debug.Log("Connected to: " + PhotonNetwork.CloudRegion);
+
+    // Create Room.
+    public void CreateRoom(string roomName) => PhotonNetwork.CreateRoom(roomName);
+    public override void OnCreatedRoom() => Debug.Log("You create a room.");
+    public override void OnCreateRoomFailed(short returnCode, string message) => Debug.Log(message);
+
+    // Join Room.
+    public void JoinRoom(string roomName) => PhotonNetwork.JoinRoom(roomName);
+    public void CancelJoinRoom() => PhotonNetwork.LeaveRoom();
+    public override void OnJoinedRoom() => Debug.Log("You joined the room");
+    public override void OnJoinRoomFailed(short returnCode, string message) => Debug.Log(message);
+
+    // Change de scene au niveau du serveur.
+    public void ChangeScene(string sceneName)
+    {
+        if (PhotonNetwork.IsMasterClient)
+            PhotonNetwork.LoadLevel(sceneName);
     }
+        
 }
