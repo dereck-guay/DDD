152ff5b95966ed512c5c79a0ccb842dcf3758655
Dereck Guay <dedeguay@gmail.com>
2020-05-10 15:00:10 -0400

    ReAdd Code/Stats/Xp.cs

---
+++ Assets/Source/Code/Stats/XP.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Miscellaneous;
+using Interfaces;
+public class XP
+{
+    int level;
+    const int MaxLevel = 3;
+    readonly float[] requiredXPPerLevel = new float[] { 15,30,50 };
+    public float Current { get; private set; }
+    public int Level
+    {
+        get 
+        {
+            return level;
+        }
+        private set
+        {
+            if (value > MaxLevel)
+                Debug.Log("Player is already max level.");
+            else
+                level = value;
+        }
+    }
+    public XP()
+    {
+        Current = 0;
+        Level = 1;
+    }
+    public void AddXP(float value)
+    {
+        if (Current + value > requiredXPPerLevel[Level - 1])
+        {
+            ++Level;
+            AddXP(value);
+            Debug.Log(value + " XP has been added");
+        }
+        else 
+        { 
+            Current += value;
+            Debug.Log(value + " XP has been added");
+        }
+    }
+    public string Name = "XP";
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(Name);
+        sb.AppendLine($"\t Current {Name} : {Current}");
+        return sb.ToString();
+    }
+}
