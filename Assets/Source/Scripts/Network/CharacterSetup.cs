a1136c6e1fc017cfb3738ab416d2c969a7ebb038
Dereck Guay <dedeguay@gmail.com>
2020-05-19 12:21:02 -0400

    Load avec le ownership

--- Assets/Source/Scripts/Network/CharacterSetup.cs
+++ Assets/Source/Scripts/Network/CharacterSetup.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
-using Photon.Pun;
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class CharacterSetup : MonoBehaviour
@@ -13,16 +15,33 @@ public class CharacterSetup : MonoBehaviour
         PV = GetComponent<PhotonView>();
 
         if (PV.IsMine)
-            PV.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.PI.mySelectedCharacter);
+        {
+            AddCharacter(PlayerInfo.PI.mySelectedCharacter);
+            PV.RPC("RPC_AddCharacter", RpcTarget.OthersBuffered, PlayerInfo.PI.mySelectedCharacter, PV.Owner);
+        }
+    }
+
+    private void AddCharacter(int whichCharacter)
+    {
+        characterValue = whichCharacter;
+        var characterName = PlayerInfo.PI.allCharacters[whichCharacter].gameObject.name;
+        myCharacter = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "ClassPrefabs", characterName), transform.position, transform.rotation, 1);
+        var pV = myCharacter.GetComponent<PhotonView>();
+        var pTV = myCharacter.AddComponent<PhotonTransformView>();
+        pV.ObservedComponents = new List<Component>();
+        pV.ObservedComponents.Add(pTV);
     }
 
     [PunRPC]
-    private void RPC_AddCharacter(int whichCharacter)
+    private void RPC_AddCharacter(int whichCharacter, Player Owner)
     {
         characterValue = whichCharacter;
-        myCharacter = Instantiate(PlayerInfo.PI.allCharacters[whichCharacter].gameObject, transform.position, transform.rotation, transform);
-        myCharacter.SetActive(true);
-        var pV = myCharacter.AddComponent<PhotonView>();
+        var characterName = PlayerInfo.PI.allCharacters[whichCharacter].gameObject.name;
+        myCharacter = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "ClassPrefabs", characterName), transform.position, transform.rotation, 1);
+        var pV = myCharacter.GetComponent<PhotonView>();
+
+        pV.TransferOwnership(Owner);
+
         var pTV  = myCharacter.AddComponent<PhotonTransformView>();
         pV.ObservedComponents = new List<Component>();
         pV.ObservedComponents.Add(pTV);
