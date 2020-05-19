6e6d82175e7c7b1b8d1f94d5d984da3456e211eb
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2020-05-19 00:14:16 -0400

    small changes and component to film

---
+++ Assets/CameraRigComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRigComponent : MonoBehaviour
+{
+    public Vector3 initPos;
+    public GameObject go;
+    Rigidbody body;
+    float currentTime;
+    public float forwardPower;
+    public float leftPower;
+    private void Awake()
+    {
+        initPos = transform.position;
+    }
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        currentTime = 1000;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(go.transform);
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            transform.position = initPos;
+            currentTime = 0;
+            body.velocity = Vector3.zero;
+            body.AddForce(Vector3.back * forwardPower);
+        }
+        if (currentTime >= 2 && currentTime < 1000)
+        {
+            body.AddForce(Vector3.right * leftPower);
+            currentTime = 1000;
+        }
+        currentTime += Time.deltaTime;
+    }
+}
